a9cd631fffc0765c3d90dbc8ef008dc356a490c9
suprita shankar <suprita.shankar@gmail.com>
2016-09-23 13:32:08 -0700

    add new templates

--- pilotmessenger/chatroom/views.py
+++ pilotmessenger/chatroom/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the chatroom index.")
+    template = loader.get_template('chatroom/index.html')
+    context = {
+        'latest_message': True,
+    }
+    return HttpResponse(template.render(context, request))
