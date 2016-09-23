9b9ee7ae3d6b663a23f0f916f628a9ab1bfc097d
suprita shankar <suprita.shankar@gmail.com>
2016-09-23 13:20:31 -0700

    use onetoone instead of foreign key field

--- pilotmessenger/chatroom/models.py
+++ pilotmessenger/chatroom/models.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
-
+import django
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Message(models.Model):
     message_text = models.CharField(max_length=200)
-    user = models.ForeignKey(User, unique=True)
+    user = models.OneToOneField(User, unique=True)
     channel = models.CharField(max_length=200)
     public_channel = models.BooleanField()
     pub_date = models.DateTimeField(default=django.utils.timezone.now)
