905ac516d10005291f3eb6b4f707a3e268b38a28
suprita shankar <suprita.shankar@gmail.com>
2016-09-23 01:25:46 -0700

    add message model and add chatroom to settings

--- pilotmessenger/chatroom/models.py
+++ pilotmessenger/chatroom/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    message_text = models.CharField(max_length=200)
+    user = models.ForeignKey(User, unique=True)
+    channel = models.CharField(max_length=200)
+    public_channel = models.BooleanField()
+    pub_date = models.DateTimeField(default=django.utils.timezone.now)
