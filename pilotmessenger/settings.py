e0767e2607d01144235689c66b0729b96ad16477
suprita shankar <suprita.shankar@gmail.com>
2016-09-26 02:37:24 -0700

    use dj database url

--- pilotmessenger/settings.py
+++ pilotmessenger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+DATABASES['default'] =  dj_database_url.config()
