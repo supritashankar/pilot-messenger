d05877f46397ef0d28b790688be9859c94838bc2
suprita shankar <suprita.shankar@gmail.com>
2016-09-26 12:35:01 -0700

    add the new keys and secret for heroku pusher add on

--- pilotmessenger/chatroom/static/js/script.js
+++ pilotmessenger/chatroom/static/js/script.js
@@ -40,7 +40,7 @@ $(document).ajaxSend(function(event, xhr, settings) {
     console.log('Script is loaded');
     Pusher.logToConsole = true;
 
-    var pusher = new Pusher('a4cc9d7318ae0879ac0b', {
+    var pusher = new Pusher('48eec20d8ef030076b17', {
       encrypted: true
     });
 
--- pilotmessenger/chatroom/views.py
+++ pilotmessenger/chatroom/views.py
@@ -60,10 +60,9 @@ class PusherClient(object):
 
     def __init__(self):
         self.pusher_client = pusher.Pusher(
-            app_id='251400',
-            key='a4cc9d7318ae0879ac0b',
-            secret='6dd89d34ce25d9ee23f4',
-            ssl=True
+            app_id='252378',
+            key='48eec20d8ef030076b17',
+            secret='d2e330e7a270983a4430'
         )
 
 def singleton(classname):
