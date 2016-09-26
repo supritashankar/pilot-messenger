586d4bdcaafefeed9b1648aeadd57aa7a74d1180
suprita shankar <suprita.shankar@gmail.com>
2016-09-26 03:03:22 -0700

    heroku was not reading the static files

--- pilotmessenger/settings.py
+++ pilotmessenger/settings.py
@@ -118,10 +118,14 @@ USE_L10N = True
 
 USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'chatroom/static')
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 DATABASES['default'] =  dj_database_url.config()
