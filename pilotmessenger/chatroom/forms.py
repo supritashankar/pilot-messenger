dbd3bd6a082617e173036f090136e6373ff3ddba
suprita shankar <suprita.shankar@gmail.com>
2016-09-25 20:46:32 -0700

    clean up the UI a bit

--- pilotmessenger/chatroom/forms.py
+++ pilotmessenger/chatroom/forms.py
@@ -1,8 +1,12 @@
 from models import Message
-from django.forms import ModelForm
+from django import forms
 
 
-class MessageForm(ModelForm):
+class MessageForm(forms.ModelForm):
      class Meta:
          model = Message
          fields = ['message_text', 'channel', 'public_channel']
+         widgets = {
+            'message_text': forms.TextInput(attrs={'class': 'form-control'}),
+            'channel': forms.TextInput(attrs={'class': 'form-control'}),
+        }
