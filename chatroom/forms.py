d7ba9eeafc854a59769dcebde079ab083d0b505f
suprita shankar <suprita.shankar@gmail.com>
2016-09-27 21:34:19 -0700

    Add events

--- chatroom/forms.py
+++ chatroom/forms.py
@@ -5,8 +5,9 @@ from django import forms
 class MessageForm(forms.ModelForm):
      class Meta:
          model = Message
-         fields = ['message_text', 'channel', 'public_channel']
+         fields = ['message_text', 'channel', 'event', 'public_channel']
          widgets = {
             'message_text': forms.TextInput(attrs={'class': 'form-control'}),
             'channel': forms.TextInput(attrs={'class': 'form-control'}),
+            'event': forms.TextInput(attrs={'class': 'form-control'}),
         }
--- chatroom/models.py
+++ chatroom/models.py
@@ -8,5 +8,6 @@ class Message(models.Model):
     message_text = models.CharField(max_length=200)
     user = models.OneToOneField(User, unique=True)
     channel = models.CharField(max_length=200)
+    event = models.CharField(max_length=200)
     public_channel = models.BooleanField()
     pub_date = models.DateTimeField(default=django.utils.timezone.now)
